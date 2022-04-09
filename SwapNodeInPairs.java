88244c5f738fb86c9367aa9aea7ac89c51a8d041
shivamlc <103298242+shivamlc@users.noreply.github.com>
2022-04-09 14:29:53 +1000

    Update SwapNodeInPairs.java

--- SwapNodeInPairs.java
+++ SwapNodeInPairs.java
@@ -15,16 +15,20 @@ class Solution {
         if(node==null||node.next==null)
             return;
         else{
-            int temp=node.val;
-            node.val=node.next.val;
-            node.next.val=temp;
-            node=node.next.next;
+            int temp=node.val;//O(1)
+            node.val=node.next.val;//O(1)
+            node.next.val=temp;//O(1)
+            node=node.next.next;//O(1)
             swap(node);
         }
     }
+    
+    //time complexity of swap(): called n/2 times where n is number of nodes. tc= n/2*O(1)=O(n/2)~=O(n)
     
     public ListNode swapPairs(ListNode head) {
         swap(head);
         return head;
     }
 }
+
+Todo: compare performance with iterative method and compute space complexity of recursion method
